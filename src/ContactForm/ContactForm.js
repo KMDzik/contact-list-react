64bc556ee0a47e046a0d22e882b95bc7f8e3f447
KMDzik <katarzyna.dzik90@gmail.com>
2018-10-13 17:52:33 +0200

    error message

--- src/ContactForm/ContactForm.js
+++ src/ContactForm/ContactForm.js
@@ -3,18 +3,37 @@ import './ContactForm.css'
 
 class ContactForm extends Component {
 
+  state = {
+    contactData: '',
+    error: null
+  }
+
 handleSubmit = event => {
   event.preventDefault()
+  if (this.state.contactData === '') {
+    this.setState({
+      error: new Error('Please add name, surename and number')
+    })
+    return;
+  }
+  this.setState({contactData: '', error: null})
+}
+
+handleChange = event => {
+  this.setState({
+    contactData: event.target.value
+  })
 }
 
   render() {
     return (
-      <div>
         <form onSubmit= {this.handleSubmit}>
-        <input />
+          {
+          this.state.error && <p>{this.state.error.message}</p>
+        }
+        <input value={this.state.contactData} onChange={this.handleChange}/>
         <button>Add</button>
       </form>
-      </div>
     )
   }
 }
