1a6b604404a9643b2982d6a4aa93c1f471b85862
KMDzik <katarzyna.dzik90@gmail.com>
2018-10-14 00:07:57 +0200

    how to create 3 forms?

--- src/ContactForm/ContactForm.js
+++ src/ContactForm/ContactForm.js
@@ -3,38 +3,98 @@ import './ContactForm.css'
 
 class ContactForm extends Component {
 
-  state = {
-    contactData: '',
-    error: null
+  state = [
+    {
+      ContactName: '',
+      error: null
+    },
+    {
+      ContactSurname: '',
+      error: null
+    },
+    {
+      ContactNumber: '',
+      error: null
+    }
+  ]
+
+  handleSubmit = event => {
+    event.preventDefault()
+    if (this.state.ContactName === '') {
+      this.setState({
+        error: new Error('Please add name, surename and number')
+      })
+      return;
+    }
+    this.props.addContactsFunction(this.state.ContactName);
+    this.setState({ ContactName: '', error: null })
   }
 
-handleSubmit = event => {
-  event.preventDefault()
-  if (this.state.contactData === '') {
+  handleChange = event => {
     this.setState({
-      error: new Error('Please add name, surename and number')
+      ContactName: event.target.value
     })
-    return;
   }
-  this.props.addContactsFunction(this.state.contactData);
-  this.setState({contactData: '', error: null})
-}
 
-handleChange = event => {
-  this.setState({
-    contactData: event.target.value
-  })
-}
+
+
+
+  handleSubmit = event => {
+    event.preventDefault()
+    if (this.state.ContactSurename === '') {
+      this.setState({
+        error: new Error('Please add name, surename and number')
+      })
+      return;
+    }
+    this.props.addContactsFunction(this.state.ContactSurename);
+    this.setState({ ContactSurename: '', error: null })
+  }
+
+  handleChange = event => {
+    this.setState({
+      ContactSurename: event.target.value
+    })
+  }
+
+
+
+
+  handleSubmit = event => {
+    event.preventDefault()
+    if (this.state.ContactNumber === '') {
+      this.setState({
+        error: new Error('Please add name, surename and number')
+      })
+      return;
+    }
+    this.props.addContactsFunction(this.state.ContactNumber);
+    this.setState({ ContactNumber: '', error: null })
+  }
+
+  handleChange = event => {
+    this.setState({
+      ContactName: event.target.value
+    })
+  }
+
+
+
 
   render() {
     return (
-        <form onSubmit= {this.handleSubmit}>
-          {
+      <form onSubmit={this.handleSubmit}>
+        {
           this.state.error && <p>{this.state.error.message}</p>
         }
-        <input value={this.state.contactData} onChange={this.handleChange}/>
-        <button>Add contact</button>
+        <input value={this.state.ContactName} onChange={this.handleChange} />
+        <button>Add name</button>
+        <input value={this.state.ContactSurename} onChange={this.handleChange} />
+        <button>Add surename</button>
+        <input value={this.state.ContactNumber} onChange={this.handleChange} />
+        <button>Add number</button>
       </form>
+
     )
   }
 }
