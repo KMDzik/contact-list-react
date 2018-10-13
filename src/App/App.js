97c92c74a7cd53c0897cd02547d1997954fdcee7
KMDzik <katarzyna.dzik90@gmail.com>
2018-10-13 15:50:09 +0200

    makeContactFavorites - onClick added

--- src/App/App.js
+++ src/App/App.js
@@ -2,51 +2,69 @@ import React, { Component } from 'react'
 
 import './App.css';
 
-const contacts = [
 
-  {
-    id: 1,
-    name: 'Aleksandra',
-    surname: 'Nowak',
-    number: '508 234 405',
-    isFavorite: true
-  },
-  {
-    id: 2,
-    name: 'Joanna',
-    surname: 'Kuc',
-    number: '987 356 387',
-    isFavorite: true
-  },
-  {
-    id: 3,
-    name: 'Zenon',
-    surname: 'Zieliński',
-    number: '598 387 467',
-    isFavorite: false
-  },
-  {
-    id: 4,
-    name: 'Kunegunda',
-    surname: 'Pieczarka',
-    number: '675 987 476',
-    isFavorite: true
-  }
-]
 class App extends Component {
+
+  state = {
+    contacts: [
+
+      {
+        id: 1,
+        name: 'Aleksandra',
+        surname: 'Nowak',
+        number: '508 234 405',
+        isFavorite: true
+      },
+      {
+        id: 2,
+        name: 'Joanna',
+        surname: 'Kuc',
+        number: '987 356 387',
+        isFavorite: true
+      },
+      {
+        id: 3,
+        name: 'Zenon',
+        surname: 'Zieliński',
+        number: '598 387 467',
+        isFavorite: false
+      },
+      {
+        id: 4,
+        name: 'Kunegunda',
+        surname: 'Pieczarka',
+        number: '675 987 476',
+        isFavorite: true
+      }
+    ]
+  }
+
+  makeContactFavorites = contactId => {
+    this.setState({
+      contacts: this.state.contacts.map(
+        contact => contactId !== contact.id ? contact : {
+          isFavorite: true
+        }
+
+      )
+    })
+  }
+
+
   render() {
     return (
       <div className="app">
         <div className="title">Contact List App</div>
         <ul> {
-          contacts.map(
+          this.state.contacts.map(
             contact => (
-              <li key={contact.id}>{contact.isFavorite && <span>&#9734;</span>} 
-              {contact.name} {contact.surname} <br /> {contact.number} 
+              <li key={contact.id}>{contact.isFavorite ? <span>&#9733;</span> : 
+                <span onClick={this.makeContactFavorites}>&#9734;</span>}
+                {contact.name} {contact.surname} <br /> {contact.number}
               </li>
             )
           )
-         }
+        }
         </ul>
       </div>
     )
