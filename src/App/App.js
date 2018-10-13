f3a88f0d84b542179e51f36c5b56df55d618c9bd
KMDzik <katarzyna.dzik90@gmail.com>
2018-10-13 14:08:37 +0200

    contact map

--- src/App/App.js
+++ src/App/App.js
@@ -2,11 +2,40 @@ import React, { Component } from 'react'
 
 import './App.css';
 
+const contacts = [
 
+  {
+    id: 1,
+    name: 'Aleksandra',
+    number: '508 234 405'
+  },
+  {
+    id: 2,
+    name: 'Joanna',
+    number: '987 356 387'
+  },
+  {
+    id: 3,
+    name: 'Zenon',
+    number: '598 387 467'
+  },
+  {
+    id: 4,
+    name: 'Kunegunda',
+    number: '675 987 476'
+  }
+]
 class App extends Component {
   render() {
     return (
-      <div> APLIKACJA KONTAKTY</div>
+      <ul> {
+        contacts.map(
+          contact => (
+            <li key={contact.id}> {contact.name} {contact.number} </li>
+          )
+        )
+      }
+      </ul>
     )
   }
 
