54e9c5122724e6e44b7a7d7d9e40ca98ff9baee6
KMDzik <katarzyna.dzik90@gmail.com>
2018-10-13 13:50:51 +0200

    array of contatcts

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 
+import './App.css';
+
+
 class App extends Component {
   render() {
     return (
-      <div> yo yo yo</div>
+      <div> APLIKACJA KONTAKTY</div>
     )
   }
 
---
+++ src/ContactList/ContactList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+import './ContactList.css';
+
+const contact = contacts [
+
+  {
+    name: 'Aleksandra',
+    number: 508 234 405
+  },
+  {
+    name: 'Joanna',
+    number:  987 356 387
+  },
+  {
+    name: 'Zenon',
+    number: 598 387 467
+  },
+  {
+    name: 'Kunegunda',
+    number: 675 987 476
+  },
+]
+
+class ContactList extends Component {
+
+  render() {
+    return (
+      <div> APLIKACJA KONTAKTY</div>
+    )
+  }
+
+}
+
+export default ContactList
